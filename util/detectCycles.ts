f6ae64210496335b34e3aed7ffdbc8a7eb9d9eb5
dtjones404 <dustintaylorjones@gmail.com>
2022-05-18 19:13:06 -0400

    Fixed bug in detectCycles.

--- util/detectCycles.ts
+++ util/detectCycles.ts
@@ -10,24 +10,29 @@ interface Graph {
 }
 
 export default function detectCycles({ nodes, edges }: Graph) {
-  const dfs = (node: string, prev: string, path: string[]) => {
-    path.push(node);
+  const dfs = (node: string, prev: string, path: string[][]) => {
     if (finished.has(node)) return;
     if (visited[node] !== undefined) {
       //cycle found
       const cycleStart = visited[node];
-      path.slice(cycleStart).forEach((node) => cycleNodes.add(node));
+      path
+        .slice(cycleStart)
+        .forEach((edge) => cycleEdges.add(JSON.stringify(edge)));
       return;
     }
-    visited[node] = path.length - 1;
-    if (adjMap[node])
-      adjMap[node].forEach((neigh) => {
-        if (neigh !== prev) dfs(neigh, node, path);
-      });
+    visited[node] = path.length;
+    if (adjMap[node]) {
+      for (const neigh of adjMap[node]) {
+        if (neigh === prev) continue;
+        path.push([node, neigh]);
+        dfs(neigh, node, path);
+        path.pop();
+      }
+    }
     finished.add(node);
   };
 
-  const cycleNodes = new Set();
+  const cycleEdges = new Set();
   const adjMap: AdjMap = {};
   for (const edge of edges) {
     const from = String(edge.from);
@@ -44,11 +49,12 @@ export default function detectCycles({ nodes, edges }: Graph) {
   });
 
   //mark nodes which are part of cycle
+  console.log(cycleEdges);
   edges.forEach((edge) => {
     const from = String(edge.from);
     const to = String(edge.to);
 
-    if (cycleNodes.has(from) && cycleNodes.has(to)) {
+    if (cycleEdges.has(JSON.stringify([from, to]))) {
       edge.color = 'red';
     }
   });
